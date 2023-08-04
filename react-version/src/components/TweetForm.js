aa49be90c5ff732e38eaf95ad288bed9df6a0893
Sandeep Gill <sandeepgill62@gmail.com>
2023-08-04 04:48:46 +0000

    Handling Events In React

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -2,7 +2,7 @@ function TweetForm () {
   const textAreaPlaceholderValue = "What are you humming about?"
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form method="post" onClick={(event) => event.preventDefault()} action="/tweets" className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder={textAreaPlaceholderValue}></textarea>
         <input type="submit" value="Tweet" className="form__input"></input>
         <span className="form__counter">140</span>
